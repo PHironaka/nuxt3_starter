685b7d2a2a86b02af258657f828cd63651582b91
phironaka <peterhironaka@gmail.com>
2023-04-19 15:12:14 -0700

    remove supabase specific code

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,10 @@
 <script setup>
-const user = useSupabaseUser()
-const { auth } = useSupabaseAuthClient()
-watchEffect(() => {
-  if (user.value)
-    navigateTo('/tasks')
-})
 </script>
 
 <template>
   <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-    <h2 class="my-6 text-center text-3xl font-extrabold u-text-white">
-      Sign in to your account
-    </h2>
-    <LoginCard>
-      <UButton
-        class="mt-3"
-        icon="i-mdi-github"
-        block
-        label="Google"
-        variant="black"
-        @click="auth.signInWithOAuth({ provider: 'google' })"
-      />
-    </LoginCard>
+    <h1 class="my-6 text-center text-3xl font-extrabold u-text-white">
+      hello
+    </h1>
   </div>
 </template>
